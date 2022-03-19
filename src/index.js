0ef6bb277a5a641c570cf1be4701f0819b3a5a3e
meyer.r.andrew@gmail.com <meyer.r.andrew@gmail.com>
2022-03-19 11:39:59 -0700

    add bookTwo

--- src/index.js
+++ src/index.js
@@ -3,41 +3,48 @@ import ReactDOM from 'react-dom';
 
 // CSS
 import './index.style.css';
+// variables
+const bookOne = {
+  cover: 'https://images.pexels.com/photos/415071/pexels-photo-415071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=250',
+  coverAlt: 'a book',
+  title: 'Coolest Book Ever',
+  author: 'Amelia Airheart',
+};
+const bookTwo = {
+  cover: 'https://images.pexels.com/photos/161366/book-book-pages-book-cover-font-161366.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+  coverAlt: 'an old book',
+  title: 'Oldest Book Ever',
+  author: 'Thug the Cavedude',
+};
+
 
 function BookList(){
   return (
     <>
       <section className='booklist'>
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book 
+          cover={bookOne.cover} 
+          coverAlt={bookOne.coverAlt} 
+          title={bookOne.title} 
+          author={bookOne.author} 
+        />
+        <Book 
+          cover={bookTwo.cover} 
+          coverAlt={bookTwo.coverAlt} 
+          title={bookTwo.title} 
+          author={bookTwo.author} 
+        />
       </section>
     </>
   );
 }
 
-const Book = () => {
+const Book = ({ cover, coverAlt, title, author }) => {
   return <article className='book'>
-    <Image />
-    <Title />
-    <Author />
+    <img src={cover} alt={coverAlt} className='w250px' />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>
-}
-
-const Image = () => {
-  return <img src="https://images.pexels.com/photos/415071/pexels-photo-415071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=250" alt="" />
-}
-
-const Title = () => {
-  return <h1>Coolest Book Ever</h1>
-}
-
-const Author = () => {
-  return <h4 style={{color:'#617d98',fontSize:'0.75rem', marginTop:'0.25rem'}}>Yours Truly</h4>
 }
  
 ReactDOM.render(<BookList/>, document.getElementById('root'));
