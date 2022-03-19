3ddc09101f73707040de0eb85d9bb2c533f3abf1
meyer.r.andrew@gmail.com <meyer.r.andrew@gmail.com>
2022-03-19 16:44:47 -0700

    seperate bookList data

---
+++ src/components/booksList.js
@@ -0,0 +1,23 @@
+export const books = [
+    {
+      id: 1,
+      cover: 'https://images-na.ssl-images-amazon.com/images/I/81SQwBAtpWL._AC_UL604_SR604,400_.jpg',
+      coverAlt: 'Run Rose Run cover',
+      title: 'Run, Rose, Run: A Novel',
+      author: 'James Patterson',
+    },
+    {
+      id: 2,
+      cover: 'https://images-na.ssl-images-amazon.com/images/I/81yE4jwLACL._AC_UL604_SR604,400_.jpg',
+      coverAlt: 'Life Force cover',
+      title: 'Life Force',
+      author: 'Tony Robbins',
+    },
+    {
+      id: 3,
+      cover: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg',
+      coverAlt: 'Atomic Habits cover',
+      title: 'Atomic Habits',
+      author: 'James Clear',
+    },
+];
